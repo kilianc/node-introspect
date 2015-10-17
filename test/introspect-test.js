e8140517015438cb19b65ceb6c282f2a47d16522
orz <orz@getstocks.com>
2015-10-17 22:32:45 +0300

    Added passing tests

--- test/introspect-test.js
+++ test/introspect-test.js
@@ -4,6 +4,16 @@ var
   assert = require('assert'),
   introspect = require('../lib/introspect/introspect');
 
+class ClassWithCtorFirst {
+  constructor(a, b) {}
+  method(c, d) {}
+}
+
+class ClassWithMethodFirst {
+  method(c, d) {}
+  constructor(a, b) {}
+}
+
 vows.describe('introspect/main').addBatch({
   'introspect should success': {
     'without spaces': function () {
@@ -39,9 +49,23 @@ vows.describe('introspect/main').addBatch({
     'with class with ctor with arguments': function () {
       assert.deepEqual(introspect(class CtorWithArgs{ constructor(foo,bar,callback){  }}), ['foo','bar','callback']);
     },
-    'with class with ineherited ctor': function () {
+    'with class with inherited ctor': function () {
       class CtorWithArgs{ constructor(foo,bar,callback){  }}
       assert.deepEqual(introspect(class InheritedCtor extends CtorWithArgs{}), ['foo','bar','callback']);
+    },
+    'with class with method after ctor': function () {
+      assert.deepEqual(introspect(ClassWithCtorFirst), ['a','b']);
+    },
+    'with class with method before ctor': function () {
+      assert.deepEqual(introspect(ClassWithMethodFirst), ['a','b']);
+    },
+    'with class unbound method': function () {
+      assert.deepEqual(introspect(ClassWithCtorFirst.prototype.method), ['c','d']);
+      assert.deepEqual(introspect(ClassWithMethodFirst.prototype.method), ['c','d']);
+    },
+    'with class bound method': function () {
+      assert.deepEqual(introspect(new ClassWithCtorFirst().method), ['c','d']);
+      assert.deepEqual(introspect(new ClassWithMethodFirst().method), ['c','d']);
     }
   }
 }).export(module);
